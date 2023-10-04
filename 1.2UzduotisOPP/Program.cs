3db047f88c6fa11eca9eb0818c3857c1673fc456
Laura <Laura@DESKTOP-BESSB6E>
2023-10-04 22:13:53 +0300

    Add project files.

---
+++ 1.2UzduotisOPP/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _1._2UzduotisOPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            School school = new School();
+            Console.WriteLine($"Pavadinimas: {school.Name} \nMiestas: {school.City}");
+        }
+    }
+}
---
+++ 1.2UzduotisOPP/School.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2UzduotisOPP
+{
+    public class School
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+    
+
+        public School() 
+        {
+            Name = "CodeAcademy";
+            City = "Vilnius";
+        }
+    }
+}
